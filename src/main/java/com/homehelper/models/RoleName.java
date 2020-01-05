f830b8eec7d9740bb799aa804fce35f4c943961b
Mikołaj Baran <tarralan@wp.pl>
2020-01-06 00:49:36 +0100

    User authorization

---
+++ src/main/java/com/homehelper/models/RoleName.java
@@ -0,0 +1,6 @@
+package com.homehelper.models;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/homehelper/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.homehelper.repository;
+
+import com.homehelper.models.Role;
+import com.homehelper.models.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
